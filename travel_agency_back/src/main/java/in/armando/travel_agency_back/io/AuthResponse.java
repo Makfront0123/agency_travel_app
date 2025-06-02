79afab789cb1bd8fff61066d6060a06814a7e25d
Jakex2018 <pusugu00@gmail.com>
2025-06-01 21:26:19 -0500

    response verified

--- travel_agency_back/src/main/java/in/armando/travel_agency_back/io/AuthResponse.java
+++ travel_agency_back/src/main/java/in/armando/travel_agency_back/io/AuthResponse.java
@@ -2,13 +2,16 @@ package in.armando.travel_agency_back.io;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
- 
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class AuthResponse {
-    private final String email;
-    private final String role;
-    private final String token;
-    private final boolean verified;
+    private String email;
+    private String role;
+    private String token;
+    private boolean verified;
 }
