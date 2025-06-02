e3f141780e871e787daab25e6baa4614f8b31029
Jakex2018 <pusugu00@gmail.com>
2025-06-01 21:41:35 -0500

    serializacion json

--- travel_agency_back/src/main/java/in/armando/travel_agency_back/io/AuthResponse.java
+++ travel_agency_back/src/main/java/in/armando/travel_agency_back/io/AuthResponse.java
@@ -1,8 +1,9 @@
 package in.armando.travel_agency_back.io;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
- 
 
 @Getter
 @AllArgsConstructor
@@ -10,5 +11,7 @@ public class AuthResponse {
     private final String email;
     private final String role;
     private final String token;
+
+    @JsonProperty("verified")
     private final boolean verified;
 }
