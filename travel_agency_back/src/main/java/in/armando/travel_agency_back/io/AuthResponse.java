2d80d1b1a08396340237d020d594aaf207f7bb2a
Jakex2018 <pusugu00@gmail.com>
2025-06-01 21:39:44 -0500

    adas

--- travel_agency_back/src/main/java/in/armando/travel_agency_back/io/AuthResponse.java
+++ travel_agency_back/src/main/java/in/armando/travel_agency_back/io/AuthResponse.java
@@ -2,16 +2,13 @@ package in.armando.travel_agency_back.io;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+ 
 
 @Getter
-@Setter
-@NoArgsConstructor
 @AllArgsConstructor
 public class AuthResponse {
-    private String email;
-    private String role;
-    private String token;
-    private boolean verified;
+    private final String email;
+    private final String role;
+    private final String token;
+    private final boolean verified;
 }
