3dabc49a14cbb9016cdcb604d7d24d814bc49c9e
Jakex2018 <pusugu00@gmail.com>
2025-06-07 17:37:19 -0500

    nnnn

--- travel_agency_back/src/main/java/in/armando/travel_agency_back/TravelAgencyBackApplication.java
+++ travel_agency_back/src/main/java/in/armando/travel_agency_back/TravelAgencyBackApplication.java
@@ -4,12 +4,12 @@ import java.util.TimeZone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"in"})
 public class TravelAgencyBackApplication {
+
     public static void main(String[] args) {
+        // Establece la zona horaria predeterminada a Colombia
         TimeZone.setDefault(TimeZone.getTimeZone("America/Bogota"));
         SpringApplication.run(TravelAgencyBackApplication.class, args);
     }
