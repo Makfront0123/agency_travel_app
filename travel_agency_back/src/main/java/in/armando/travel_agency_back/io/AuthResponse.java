d301f5471ab2834652b6dbeb64c9adc19cda3f06
Jakex2018 <pusugu00@gmail.com>
2025-06-07 11:44:42 -0500

    yyyyy

--- travel_agency_back/src/main/java/in/armando/travel_agency_back/io/AuthResponse.java
+++ travel_agency_back/src/main/java/in/armando/travel_agency_back/io/AuthResponse.java
@@ -1,17 +1,22 @@
 package in.armando.travel_agency_back.io;
 
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+
+ 
+
+@JsonInclude(JsonInclude.Include.ALWAYS)
 @Getter
 @AllArgsConstructor
 public class AuthResponse {
-
     private final String email;
     private final String role;
     private final String token;
-
     private final boolean verified;
 }
+
 
