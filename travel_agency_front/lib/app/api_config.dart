d467fedf87a416ff77d539c8f1afc531f4f89b59
Jakex2018 <pusugu00@gmail.com>
2025-06-07 19:13:52 -0500

    api config frontend deploy

--- travel_agency_front/lib/app/api_config.dart
+++ travel_agency_front/lib/app/api_config.dart
@@ -1,30 +1,6 @@
 import 'dart:io';
 
 class ApiConfig {
-  static String get baseUrl {
-    if (Platform.isAndroid) {
-      // Detecta si está corriendo en un emulador
-      final isEmulator = _isRunningOnEmulator();
-      return isEmulator
-          ? 'http://10.0.2.2:8080' // Emulador Android accede al host así
-          : 'http://192.168.1.89:8080'; // IP local de tu PC
-    } else if (Platform.isIOS) {
-      return 'http://localhost:8080'; // iOS emulador puede usar localhost
-    } else {
-      return 'http://192.168.1.89:8080'; // fallback para otros entornos
-    }
-  }
-
-  static bool _isRunningOnEmulator() {
-    return !Platform.environment.containsKey("ANDROID_STORAGE");
-  }
-}
-
-
-/*
-import 'dart:io';
-
-class ApiConfig {
   static const bool isProduction = true;
 
   static String get baseUrl {
@@ -32,6 +8,7 @@ class ApiConfig {
       return 'https://agency-travel-app.onrender.com';
     }
 
+    // Modo desarrollo
     if (Platform.isAndroid) {
       final isEmulator = _isRunningOnEmulator();
       return isEmulator ? 'http://10.0.2.2:8080' : 'http://192.168.1.89:8080';
@@ -46,28 +23,3 @@ class ApiConfig {
     return !Platform.environment.containsKey("ANDROID_STORAGE");
   }
 }
- */
-
-/*
-import 'dart:io';
-
-class ApiConfig {
-  static String get baseUrl {
-    if (Platform.isAndroid) {
-      // Detecta si está corriendo en un emulador
-      final isEmulator = _isRunningOnEmulator();
-      return isEmulator
-          ? 'http://10.0.2.2:8080' // Emulador Android accede al host así
-          : 'http://192.168.1.89:8080'; // IP local de tu PC
-    } else if (Platform.isIOS) {
-      return 'http://localhost:8080'; // iOS emulador puede usar localhost
-    } else {
-      return 'http://192.168.1.89:8080'; // fallback para otros entornos
-    }
-  }
-
-  static bool _isRunningOnEmulator() {
-    return !Platform.environment.containsKey("ANDROID_STORAGE");
-  }
-}
- */
