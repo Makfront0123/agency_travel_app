e9f8aeb6d4aae30ec85678c78f8a1eca79275980
Jakex2018 <pusugu00@gmail.com>
2025-06-07 23:00:27 -0500

    cors

--- travel_agency_back/src/main/java/in/armando/travel_agency_back/config/SecurityConfig.java
+++ travel_agency_back/src/main/java/in/armando/travel_agency_back/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -17,8 +16,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 import in.armando.travel_agency_back.filter.JwtRequestFilter;
 import in.armando.travel_agency_back.service.impl.UserDetailService;
@@ -33,46 +32,37 @@ public class SecurityConfig {
         private final JwtRequestFilter jwtRequestFilter;
 
         @Bean
-        public CorsFilter showCors() {
-                return new CorsFilter(corsConfig());
-        }
-
-        private UrlBasedCorsConfigurationSource corsConfig() {
-                CorsConfiguration corsConfiguration = new CorsConfiguration();
-                corsConfiguration.setAllowedOrigins(List.of(
+        public CorsConfigurationSource corsConfigurationSource() {
+                CorsConfiguration configuration = new CorsConfiguration();
+                configuration.setAllowedOrigins(List.of(
                                 "http://localhost:3000",
                                 "http://127.0.0.1:5500",
                                 "http://192.168.1.89:8080",
                                 "http://10.0.2.2:8080",
                                 "https://agency-travel-app-2tvz.vercel.app"));
-                                
-
-                corsConfiguration.setAllowCredentials(true);
-                corsConfiguration.setAllowedMethods(
-                                List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-                corsConfiguration.setAllowedHeaders(
-                                List.of("Content-Type", "Authorization"));
+                configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                configuration.setAllowedHeaders(List.of("Content-Type", "Authorization"));
+                configuration.setAllowCredentials(true);
 
                 UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-                source.registerCorsConfiguration("/**", corsConfiguration);
+                source.registerCorsConfiguration("/**", configuration);
                 return source;
         }
 
         @Bean
         public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
                 http
-                                .cors(Customizer.withDefaults())
+                                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                                 .csrf(AbstractHttpConfigurer::disable)
                                 .authorizeHttpRequests(auth -> auth
-                                                .requestMatchers("/login", "/encode", "/register", "/verify",
-                                                                "/resend-otp", "/forgot", "/verifyForgot",
-                                                                "/reset-password",
-                                                                "/test-auth", "/debug-user") // <--- agrega este
+                                                .requestMatchers(
+                                                                "/login", "/encode", "/register", "/verify",
+                                                                "/resend-otp",
+                                                                "/forgot", "/verifyForgot", "/reset-password",
+                                                                "/test-auth", "/debug-user")
                                                 .permitAll()
-
                                                 .requestMatchers("/airport/**", "/flight/**", "/reservation/**",
-                                                                "/details/**", "/payment/**",
-                                                                "/users/**")
+                                                                "/details/**", "/payment/**", "/users/**")
                                                 .hasAnyRole("USER", "ADMIN")
                                                 .requestMatchers("/admin/**").hasRole("ADMIN")
                                                 .anyRequest().authenticated())
@@ -95,5 +85,4 @@ public class SecurityConfig {
                 provider.setPasswordEncoder(passwordEncoder());
                 return new ProviderManager(provider);
         }
-
 }
