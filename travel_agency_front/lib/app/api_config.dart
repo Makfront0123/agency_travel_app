9b4ba85bfdf2f7d9da1f7c70b6957c8c86dd83ed
Jakex2018 <pusugu00@gmail.com>
2025-06-09 14:04:50 -0500

    jkjk

--- travel_agency_front/lib/app/api_config.dart
+++ travel_agency_front/lib/app/api_config.dart
@@ -1,5 +1,2 @@
 // api_config.dart
-// ignore: unused_import
-import 'api_config_android.dart' if (dart.library.html) 'api_config_web.dart';
-
-export 'api_config_android.dart' if (dart.library.html) 'api_config_web.dart';
+export 'api_config_web.dart' if (dart.library.io) 'api_config_android.dart';
