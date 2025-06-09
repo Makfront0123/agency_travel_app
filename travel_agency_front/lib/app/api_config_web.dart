298423b102a18d2273a84d60d9dae5282b7996da
Jakex2018 <pusugu00@gmail.com>
2025-06-08 23:05:50 -0500

    asd

--- travel_agency_front/lib/app/api_config_web.dart
+++ travel_agency_front/lib/app/api_config_web.dart
@@ -2,7 +2,6 @@ class ApiConfig {
   static const bool isProduction = true;
 
   static String get baseUrl {
-    // Siempre usa producción en web o puedes poner URL local si quieres.
     return 'https://agency-travel-app.onrender.com';
   }
 }
